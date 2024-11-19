b53a653a237e4d8ae8983f7bcf3f4def31bb7e5c
Maksims Maļinovskis <118106919+maximalian@users.noreply.github.com>
2024-11-19 14:23:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,10 +27,31 @@ def calculate():
 	height = float(request.form['height'])
 	weight = float(request.form['weight'])
 	gender = request.form['gender']
+	if gender == 'man':
+		gender = 1
+	else:
+		gender = 0
+
+	categories = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17']
+	checkbox_values={category: False for category in categories}
+	checked_categories = request.form.getlist('category')
+	for category in checked_categories:
+		if category in checkbox_values:
+			checkbox_values[category] = True
+	checkbox_array = [checkbox_values[category] for category in categories]
+
+	productType = request.form['type']
+	veganCheck = request.form.get('vegan')
+	vegan = 0
+	if veganCheck == 'vegan':
+		vegan = 1
+	Type = 0
+	if productType == 'Select categories':
+		Type = 1
+
 
 	# Call the calculate_diet function with the user inputs and store the result
-	results = DIETcalc.calculate_diet(age, height, weight, gender)  # Call the function with inputs
-
+	results = DIETcalc.calculate_diet(age, height, weight, gender,vegan,Type,checkbox_array)  # Call the function with inputs
 	if results:
 		# Render the result.html template with the calculation results
 		return render_template('result.html', results = results)
@@ -40,4 +61,4 @@ def calculate():
 
 if __name__ == '__main__':
 	# Run the Flask application on the specified host and port
-	app.run("0.0.0.0", port = 80, debug = True)
+	app.run("0.0.0.0", port = 5001, debug = True)
